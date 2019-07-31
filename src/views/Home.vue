2d852d03965663f087fc55d201aeab496f56464d
yamileon <yamileon.xfz@gmail.com>
2019-07-31 13:12:57 +0100

    ui mvd done
    need to integrate to api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input v-model="inname" placeholder="insert name here" />
+    <button @click="searchname(inname)">search</button>
+    <p>pokemon name : {{pokename}}</p>
+    <p>id : {{pokeid}}</p>
+    <p>ablity : {{pokeablity}}</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import homec from "@/components/homec.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    HelloWorld
+    homec
+  },
+  data() {
+    return {
+      iname: "",
+      pokename: "",
+      pokeid: "",
+      pokeablity: ""
+    };
+  },
+  methods: {
+    searchname(pname) {
+      console.log(process.env.VUE_APP_APiCON)
+      console.log(pname);
+      this.$http.get().then(
+        response => {
+          // this.someData = response.body;
+          // console.log(this.someData)
+        },
+        response => {
+          // error callback
+        }
+      );
+    }
   }
-}
+};
 </script>
